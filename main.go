cf2f6808bc7d7f1cb87ccc4b3b04c446263cdfe6
Pranoy Kundu <pranoy1998k@gmail.com>
2022-10-27 10:16:33 +0530

    add steps to start service

--- main.go
+++ main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"fmt"
+	"os/exec"
+)
+
+func main()  {
+	_, err := exec.Command("service", "urlshortner", "start").Output()
+	if err != nil {
+		fmt.Println("start service failed :: ", err.Error())
+	}
+
+	_, err = exec.Command("service", "urlshortner", "enable").Output()
+	if err != nil {
+		fmt.Println("start service failed :: ", err.Error())
+	}
+}
